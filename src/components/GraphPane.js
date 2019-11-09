d478584a0674c810c434054482d7e0487d6f8d26
Adam Binks <jellyberg@gmail.com>
2019-11-09 00:13:46 +0000

    Add arrows, panzoom, add nodes

--- src/components/GraphPane.js
+++ src/components/GraphPane.js
@@ -1,84 +1,43 @@
 import React from 'react';
 import './GraphPane.css';
 import SVG from 'svg.js';
+import 'svg.panzoom.js';
 import 'svg.draggy.js';
 import 'svg.connectable.js';
-import MouseFollower from './mouse_follower.js'
+import MouseFollower from './MouseFollower.js'
+import GraphNode from './GraphNode.js'
 
 class GraphPane extends React.Component {
     componentDidMount() {
         this.$el = document.querySelector("#graph")
 
-        var svg = new SVG(this.$el).size("100%", "100%");
+        var svg = new SVG(this.$el).size("100%", "100%").panZoom({zoomFactor: 0.1, zoomMin: 0.5, zoomMax: 1.5});
         var links = svg.group();
         var markers = svg.group();
         var nodes = svg.group();
 
-        var mouse_follower = new MouseFollower(this.$el, svg, links, markers);
+        var mouse_follower = new MouseFollower(this.$el, svg, links, markers, links);
 
-        var g1 = nodes.group().translate(300, 100);
+        // add new nodes on mouse click
+        svg.on('dblclick', (e) => {
+            // only clicks on background and not when dragging
+            if (e.target !== svg.node && !mouse_follower.drawing_arrow_from) {
+                return;
+            }
 
-        var arrow_hitbox_margin = 20;
-        var arrow_hitbox = g1.rect(100 + 2 * arrow_hitbox_margin, 80 + 2 * arrow_hitbox_margin)
-                .translate(-arrow_hitbox_margin, -arrow_hitbox_margin).opacity(0);
-        var rect = g1.rect(100, 80).radius(10).fill("#C2185B").draggy();
-        rect.on('dragmove', (event) => {
-            g1.translate(event.detail.delta.movedX, event.detail.delta.movedY);
-            // rect.move(0, 0);
+            var point = svg.point(e.clientX, e.clientY);
+            new GraphNode(nodes, mouse_follower, "", point.x, point.y, 100, 80);
+        });
+
+        svg.on('panStart', (e) => {
+            document.activeElement.blur();
         })
-        var text = g1.text("Node 1");
-        rect.click(function () {
-            document.activeElement.blur(); // remove focus from everything
 
-            var textarea = document.querySelector('#nodeedit');
-            textarea.value = text.text();
-            textarea.style.left = g1.x() + "px";
-            textarea.style.top = g1.y() + "px";
-            textarea.style.display = "inline-block";
-            textarea.focus();
-            var save_changes = function () {
-                text.text(textarea.value);
-            }
-            var save_and_hide = function () {
-                save_changes();
-                textarea.style.display = "none";
-            }
-            textarea.onblur = save_and_hide;
-            textarea.onkeyup = function (e) {
-                save_changes();
-                if (e.key === "Escape" || e.key === "Esc" || e.key === "Enter") {
-                    save_and_hide();
-                }
-            }
-        });
-
-        arrow_hitbox.on('mouseenter', function() {
-            mouse_follower.update_source(g1);
-        });
-
-        arrow_hitbox.on('mouseleave', function() {
-            mouse_follower.hide();
-        });
-
-        var g2 = nodes.group().translate(100, 100).draggy();
-        g2.circle(50).fill("#E91E63");
-
-        var g3 = nodes.group().translate(200, 300).draggy();
-        g3.circle(100).fill("#FF5252");
-
-        g1.connectable({
-            container: links,
-            markers: markers
-        }, g2).setLineColor("#5D4037");
-
-        g2.connectable({
-            container: links,
-            markers: markers
-        }, g1).setLineColor("#5D4037");
-
-        g2.connectable({
-            padEllipse: true
-        }, g3).setLineColor("#5D4037");
+        // temp
+        new GraphNode(nodes, mouse_follower, "Cool", 300, 100, 100, 80);
+        new GraphNode(nodes, mouse_follower, "Nice", 400, 200, 100, 80);
+        new GraphNode(nodes, mouse_follower, "Kench", 100, 400, 100, 80);
+        new GraphNode(nodes, mouse_follower, "Tight", 350, 600, 100, 80);
     }
 
     componentWillUnmount() {
---
+++ src/components/MouseFollower.js
@@ -0,0 +1,70 @@
+const COMPLETED_ARROW_COLOUR = "#5D4037";
+const POTENTIAL_ARROW_COLOUR = "#d4b8b0";
+const DRAWING_ARROW_COLOUR = "#719deb";
+
+class MouseFollower {
+    constructor(parent_element, svg, connectables_container, markers, links) {
+        this.drawing_arrow_from = false;
+        this.connector = undefined;
+        this.connectables_container = connectables_container;
+        this.markers = markers;
+        this.links = links;
+
+        this.mouse_rect = svg.group();
+        window.addEventListener('mousemove', e => {
+            this.mouse_rect.move(e.clientX - parent_element.getBoundingClientRect().left, e.clientY - parent_element.getBoundingClientRect().top);
+            
+            if (typeof(this.connector) !== 'undefined') {
+                this.connector.update();
+            }
+        });
+
+        // cancel arrows if they don't end on a node
+        window.addEventListener('mouseup', (e) => {
+            if (this.drawing_arrow_from) {
+                e.preventDefault();
+                this.stop_drawing_arrow();
+            }
+        });
+    }
+
+    update_source(new_source) {
+        if (typeof(this.connector) === 'undefined') {
+            this.connector = new_source.connectable({
+                container: this.connectables_container,
+                markers: this.markers
+            }, this.mouse_rect);
+        } else {
+            this.connector.target = this.mouse_rect;
+            this.connector.source = new_source;
+            this.connector.update();
+        }
+        this.connector.setLineColor(POTENTIAL_ARROW_COLOUR);
+    }
+
+    hide() {
+        if (typeof(this.connector) !== 'undefined') {
+            this.connector.target = this.connector.source;
+        }
+    }
+
+    start_arrow(source_node) {
+        this.drawing_arrow_from = source_node;
+        this.connector.setLineColor(DRAWING_ARROW_COLOUR);
+    }
+
+    complete_arrow(end_node) {
+        this.drawing_arrow_from.group.connectable({
+            container: this.links,
+            markers: this.markers
+        }, end_node.group).setLineColor(COMPLETED_ARROW_COLOUR);
+        this.stop_drawing_arrow()
+    }
+
+    stop_drawing_arrow() {
+        this.drawing_arrow_from = false;
+        this.hide();
+    }
+}
+
+export default MouseFollower;
