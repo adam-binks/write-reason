d504a4247296c7e4539a702c9e89c45bfaffb0d1
Adam Binks <jellyberg@gmail.com>
2019-12-10 17:20:51 +0000

    bidirectional text editing woo

--- src/components/doc/NodePlugin.js
+++ src/components/doc/NodePlugin.js
@@ -15,6 +15,7 @@ export default function LinkPlugin(options) {
         renderInline(props, editor, next) {
             if (props.node.type === 'link') {
                 var id = props.node.data.get("node_id");
+                editor.getSharedState().setLinkMapping(id, props.node);
                 var ref = React.createRef();
                 editor.getSharedState().registerLinkNode(id, ref);
 
@@ -27,6 +28,21 @@ export default function LinkPlugin(options) {
         onDrop(event, editor, next) {
             console.log('drop');
             next();
+        },
+
+        onChange(editor, next) {
+            const { value } = editor;
+            const { document, selection } = value;
+            var inlines = document.getLeafInlinesAtRange(selection);
+            if (inlines) {
+                inlines.forEach(inline => {
+                    var fullText = "";
+                    inline.getTexts().forEach(text => {
+                        fullText += text.text;
+                    })
+                    editor.getSharedState().updateGraphShortText(inline.data.get("node_id"), fullText);
+                });
+            }
         }
     }
 }
