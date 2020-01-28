01e1d75e8e1492daa20ce93aed6b4b675daef4ec
Adam Binks <ab390@st-andrews.ac.uk>
2020-01-28 15:16:34 +0000

    improved optionpopups

--- src/components/graph/OptionPopup.js
+++ src/components/graph/OptionPopup.js
@@ -1,26 +1,34 @@
 export default class OptionPopup {
-    constructor(entries, x, y, callback) {
+    constructor(entries, x, y, shouldHideOnClickOutside, callback, selected=undefined) {
         var div = document.createElement("div");
         div.classList.add("option-popup-div");
-        div.appendChild(this.generateTable(entries, div, callback));
+        div.appendChild(this.generateTable(entries, div, callback, selected));
         div.style.left = x + "px";
         div.style.top = y + "px";
 
         document.getElementById("graph").appendChild(div);
+
+        if (shouldHideOnClickOutside) {
+            setTimeout(() => this.hideOnClickOutside(div), 0.01);
+        }
     }
 
-    generateTable(entries, div, callback) {
+    generateTable(entries, div, callback, selected) {
         var table = document.createElement("table");
         entries.forEach(entry => {
             var row = table.insertRow();
             row.classList.add("option-row");
+            if (entry.name === selected) {
+                row.classList.add("option-row-selected");
+            }
+
             row.addEventListener("click", e => {
                 callback(entry);
                 div.remove();
             });
 
             var colourCell = row.insertCell();
-            colourCell.appendChild(document.createTextNode("→"));
+            colourCell.appendChild(document.createTextNode(entry.symbol));
             colourCell.style.color = entry.colour;
             colourCell.style.fontSize = "200%";
 
@@ -29,5 +37,22 @@ export default class OptionPopup {
         });
 
         return table;
+    }
+
+    hideOnClickOutside(element) {
+        const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length );
+
+        const outsideClickListener = event => {
+            if (!element.contains(event.target) && isVisible(element)) {
+              element.remove();
+              removeClickListener()
+            }
+        }
+    
+        const removeClickListener = () => {
+            document.removeEventListener('click', outsideClickListener)
+        }
+    
+        document.addEventListener('click', outsideClickListener)
     }
 }
