3839482fe8f472f5bd8a3a12f93454050bf25f4b
Adam Binks <jellyberg@gmail.com>
2020-04-14 14:10:12 +0100

    final analysis

--- analysis/const.py
+++ analysis/const.py
@@ -1,2 +1,4 @@
-DATA_DIR = '/cs/home/ab390/Documents/sh_data' # 'C:\\Users\\jelly\\Desktop\\sh_data'
-QUESTIONNAIRE_RESPONSES_FILE = '/cs/home/ab390/Documents/sh_misc_data/questionnaire_responses.csv'
+DATA_DIR = 'C:\\Users\\jelly\\Documents\\sh_data\\sh_data'  # '/cs/home/ab390/Documents/sh_data'
+SCORES_DATA_DIR = 'C:\\Users\\jelly\\Documents\\sh_data\\sh_score_data'
+QUESTIONNAIRE_RESPONSES_FILE = 'C:\\Users\\jelly\\Documents\\sh_data\\questionnaire_responses.csv' # '/cs/home/ab390/Documents/sh_misc_data/questionnaire_responses.csv'
+GRAPH_CATEGORISATION_FILE = 'C:\\Users\\jelly\\Documents\\sh_data\\graph_categorisation.csv'
