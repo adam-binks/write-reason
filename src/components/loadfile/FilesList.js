f839847c312ad27eee2d60f67780aa35a0e95799
Adam Binks <jellyberg@gmail.com>
2020-05-08 13:46:30 +0100

    load file page

---
+++ src/components/loadfile/FilesList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class FilesList extends Component {
+    
+    render() {
+        return (
+            <table className='pure-table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.props.projects.map(project => <tr key={project.id.toString()}>
+                            <td>{project.id}</td>
+                            <td>{project.name}</td>
+                            <td><button className="pure-button" onClick={() => this.props.loadProject(project.id)}>Open</button></td>
+                            <td><button className="pure-button" onClick={() => this.props.deleteProject(project.id)}>Delete</button></td>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
+        );
+      }
+}
