e886a665183ba07d525da9c94bcbdc071880f357
Adam Binks <jellyberg@gmail.com>
2020-05-11 09:59:58 +0100

    Updates

---
+++ precache-manifest.6d06802a718dc45928919921d9aee70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc06d0cf3aeed682c9a09b3303d9580",
+    "url": "/write-reason/index.html"
+  },
+  {
+    "revision": "5726ea1558bf3478ca04",
+    "url": "/write-reason/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "6f6b693d1b2662da03a9",
+    "url": "/write-reason/static/css/main.323967a8.chunk.css"
+  },
+  {
+    "revision": "5726ea1558bf3478ca04",
+    "url": "/write-reason/static/js/2.2640db3d.chunk.js"
+  },
+  {
+    "revision": "6f6b693d1b2662da03a9",
+    "url": "/write-reason/static/js/main.52655372.chunk.js"
+  },
+  {
+    "revision": "f0ca42256b6c3b71c3f4",
+    "url": "/write-reason/static/js/runtime-main.b9c216d1.js"
+  }
+]);
