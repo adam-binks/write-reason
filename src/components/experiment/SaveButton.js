0c1d6767322c4dbfc4efdf63532ebc7bc4b7b8a8
Adam Binks <jellyberg@gmail.com>
2020-07-03 12:35:21 +0100

    update experimental controls for new study

--- src/components/experiment/SaveButton.js
+++ src/components/experiment/SaveButton.js
@@ -6,7 +6,8 @@ export default class ExperimentControls extends Component {
         return (
             <div className="experiment-controls">
                 <button className='pure-button' onClick={this.props.backToMenu}>Back</button>
-                <button className='pure-button' onClick={this.props.showExperimentInfo}>Study participant info</button>
+                <button className='pure-button' onClick={this.props.showExperimentInfo}>Study participant instructions</button>
+                <button className='pure-button' onClick={this.props.sharedState.viewFactSheet}>View fact sheet</button>
                 <button className='pure-button' onClick={this.props.sharedState.exportPDF}>Export PDF</button>
                 <button className='pure-button' onClick={this.props.sharedState.downloadExperimentData}>Export logs</button>
                 <button className='pure-button button-primary' onClick={() => {this.props.sharedState.save()}}>Save</button>
