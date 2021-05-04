695de3460cf50f83747f09dc4e1c1acc316c0e67
Adam Binks <jellyberg@gmail.com>
2021-05-04 11:18:46 +0100

    prep

--- analysis/const.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DATA_ROOT = 'C:/Users/jelly/Documents/sh_data' #'./../data'
-DATA_DIR = os.path.join(DATA_ROOT, 'sh_data')
-SCORES_DATA_DIR = os.path.join(DATA_ROOT, 'sh_score_data')
-QUESTIONNAIRE_RESPONSES_FILE = os.path.join(DATA_ROOT, 'questionnaire_responses.csv')
-GRAPH_CATEGORISATION_FILE = os.path.join(DATA_ROOT, 'graph_categorisation.csv')
